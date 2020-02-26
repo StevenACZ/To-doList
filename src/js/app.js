1c3f12f1ee1c7aba8b8fa8d7b6eb7f3919d94ff0
Steven Coaila Zaa <scoailazaa@gmail.com>
2020-02-26 18:14:40 -0500

    JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,75 @@
-let btnAddTask = document.querySelector('.input-task__btn');
+//SELECT ELEMENTS AND ASSIGN THEM TO VARS
+var inputNewTask = document.querySelector('#inputNewTask');
+var btnAddTask = document.querySelector('#btnAddTask');
+
+var toDoUl = document.querySelector(".todo-list ul");
+var completeUl =  document.querySelector(".complete-list ul");
+
+
+let tasks = [];
+let indexTasks = 0;
+
+btnAddTask.addEventListener('click', function() {
+  var listItem = document.createElement("li"); //<li>
+  var checkBox = document.createElement("input"); //checkbox
+  var label = document.createElement("label"); // <label>
+  let btnRemove = document.createElement('button') // <button>
+  
+  listItem.value = indexTasks;
+  
+  label.innerText = inputNewTask.value;
+  
+  checkBox.type = "checkbox";
+  checkBox.value = indexTasks;
+  checkBox.classList.add("chk");
+  
+  btnRemove.textContent = 'Delete';
+  btnRemove.value = indexTasks;
+  btnRemove.id = 'btnDelete';
+  btnRemove.onclick = function() { 
+    // of todolist to completelist console.log(completeUl.appendChild(this.parentElement));
+    // remove this.parentElement.remove();
+   };
+  
+  listItem.appendChild(checkBox);
+  listItem.appendChild(label);
+  listItem.appendChild(btnRemove);
+  
+  toDoUl.appendChild(listItem);
+  
+  tasks.push(listItem);
+  indexTasks++;
+  
+  console.log(tasks);
+  console.log(listItem);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let btnAddTask = document.querySelector('.input-task__btn');
 let tasksActives = document.querySelector('.activities-days');
-let tasksCounter = 0;
+let incompleteTasksCounter = 0;
+let completeTasksCounter = 0;
 
 let completeTasks = [];
 let incompleteTasks = [];
@@ -14,6 +83,8 @@ btnAddTask.addEventListener('click', function() {
   let left = document.createElement('div');
   left.classList.add('left');
   let btnComplete = document.createElement('a');
+  btnComplete.value = 23;
+  console.log(btnComplete);
   let iconCheck = document.createElement('i');
   iconCheck.classList.add('far');
   iconCheck.classList.add('fa-check-circle');
@@ -38,22 +109,11 @@ btnAddTask.addEventListener('click', function() {
   right.appendChild(btnDelete);
   btnDelete.appendChild(iconTrash);
 
-  /*newTask.innerHTML = ` <div class="task">
-                          <div class="left">
-                            <a href="#" value="${tasksCounter}" id="complete"><i class="far fa-check-circle"></i></a>
-                            <p class="heading-primary">${valueInput}</p>
-                          </div>
-
-                          <div class="right">
-                            <a href="#"><i class="fas fa-trash-alt"></i></a>
-                          </div>
-                        </div>`;*/
-
   let tasksContainer = document.querySelector('.tasks');
   tasksContainer.appendChild(newTask);
 
-  tasksCounter++;
-  tasksActives.textContent = tasksCounter;
+  incompleteTasksCounter++;
+  tasksActives.textContent = incompleteTasksCounter;
 
   incompleteTasks.push(newTask);
   console.log(newTask);
