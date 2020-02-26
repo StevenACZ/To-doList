18d4effb0b48266fa24b8fb95880fe1b6a435a7d
Steven Coaila Zaa <scoailazaa@gmail.com>
2020-02-25 23:14:40 -0500

    adding more JS code

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,25 @@
-let valueInput = document.querySelector('.input-task__input').value;
 let btnAddTask = document.querySelector('.input-task__btn');
+let tasksActives = document.querySelector('.activities-days');
+let tasksCounter = 0;
 
 btnAddTask.addEventListener('click', function() {
+  let valueInput = document.querySelector('.input-task__input').value;
+
   let newTask = document.createElement('div');
-  newTask.classList.add('task');
-  newTask.textContent = valueInput;
+  newTask.innerHTML = ` <div class="task">
+                          <div class="left">
+                            <a href="#"><i class="far fa-check-circle"></i></a>
+                            <p class="heading-primary">${valueInput}</p>
+                          </div>
+
+                          <div class="right">
+                            <a href="#"><i class="fas fa-trash-alt"></i></a>
+                          </div>
+                        </div>`;
 
   let tasksContainer = document.querySelector('.tasks');
   tasksContainer.appendChild(newTask);
-  console.log(newTask);
+
+  tasksCounter++;
+  tasksActives.textContent = tasksCounter;
 })
